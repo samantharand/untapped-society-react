c0f0352b2202bbba77e3b05c110942e6b8f65854
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-07-12 16:46:10 -0500

    oauth google button appears, appears to allow login

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import {
   Route,
   Link
 } from "react-router-dom"
+import GoogleBtn from './GoogleBtn'
 
 function App() {
 
   return (
-    <Router className="App">
-      <Switch>
-        <Route path="/admin">
-          <AdminManager/>
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <GoogleBtn />
+      <Router className="App">
+        <Switch>
+          <Route path="/admin">
+            <AdminManager/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
