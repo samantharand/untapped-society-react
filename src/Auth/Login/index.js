34d1a4d6c6a4d6d566ad0df1748f3ef941a12c22
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-07-13 13:57:59 -0500

    user login and register are successful

--- src/Auth/Login/index.js
+++ src/Auth/Login/index.js
@@ -6,19 +6,83 @@ export default class Login extends Component {
     super()
 
     this.state = {
-      loginForm: true,
       username: '',
       password: '',
-
     }
   }
 
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  // API call
+  login = async (loginInfo) => {
+    console.log('login');
+    const url = "http://localhost:8000/api/v1/users/login"
+
+    try {
+      
+      const loginResult = await fetch(url, {
+        credentials: 'include',
+        method: "POST",
+        body: JSON.stringify(loginInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      console.log('loginResult', loginResult);
+
+      const loginJson = await loginResult.json()
+
+      // return loginJson
+      console.log('loginJson', loginJson);
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // event handler
+  onLogin = async (e) => {
+    e.preventDefault()
+    console.log('onLogin');
+    try {
+      
+      const result = await this.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   render() {
+    const { username, password } = this.state
     return (
       <React.Fragment>
+        <form onSubmit={ this.onLogin }>
+          <p> Login </p>
+          <label> Username </label>
+          <input 
+            placeholder='Username'
+            name='username'
+            value={ username }
+            onChange={ this.onChange }
+          />
+          <label> Password </label>
+          <input 
+            placeholder='Password'
+            type='password'
+            name='password'
+            value={ password }
+            onChange={ this.onChange }
+          />
+          <button> Login </button>
+        </form>
+        <p> Or login using google </p>
         <GoogleBtn />
-        <p> Login </p>
       </React.Fragment>
     )
   }
--- src/Auth/Register/index.js
+++ src/Auth/Register/index.js
@@ -6,20 +6,88 @@ export default class Register extends Component {
   	super()
 
   	this.state = {
-  		loginForm: true,
   		username: '',
   		password: '',
-
+      checkPassword: ''
   	}
   }
 
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  // API call
+  register = async (registerInfo) => {
+    const url = "http://localhost:8000/api/v1/users/create"
+
+    try {
+      
+      const registerResult = await fetch(url, {
+        credentials: 'include',
+        method: "POST",
+        body: JSON.stringify(registerInfo),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      // console.log('registerResult');
+      const registerJson = await registerResult.json()
+      console.log('registerJson', registerJson);
+      // return registerJson
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // event handler
+  onRegister = async (e) => {
+    e.preventDefault()
+    if (this.state.password !== this.state.checkPassword){
+      console.log('passwords dont match');
+    } else {
+      const result = await this.register({
+        username: this.state.username,
+        password: this.state.password,
+        jobseeker: true
+      })
+    }
+  }
 
   render() {
-  	return (
-	  	<React.Fragment>
-	  		<GoogleBtn />
-	  		<p> Reg </p>
-	  	</React.Fragment>
-  	)
+    const { username, password, checkPassword } = this.state
+    return (
+      <React.Fragment>
+        <form onSubmit={ this.register }>
+          <p> Register </p>
+          <label> Username </label>
+          <input 
+            placeholder='Username'
+            name='username'
+            value={ username }
+            onChange={ this.onChange }
+          />
+          <label> Password </label>
+          <input 
+            placeholder='Password'
+            type='password'
+            name='password'
+            value={ password }
+            onChange={ this.onChange }
+          />
+          <label> Confirm Password </label>
+          <input 
+            placeholder='Confirm Password'
+            type='password'
+            name='checkPassword'
+            value={ checkPassword }
+            onChange={ this.onChange }
+          />
+          <button onClick={ this.onRegister }> Register </button>
+        </form>
+        <p> Or register using google </p>
+        <GoogleBtn />
+      </React.Fragment>
+    )
   }
 }
