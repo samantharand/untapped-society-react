a4310436707d8af68eed9a8ba0c4d7ae3e8f456e
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-07-14 17:27:44 -0400

    set up job board

---
+++ src/JobBoardManager/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class JobBoardManager extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      "some stuff"
+    )
+  }
+}
