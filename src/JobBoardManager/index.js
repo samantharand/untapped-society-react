b86069c5c0cb4c588527fce79a75a2b7496477fb
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-07-15 18:01:45 -0400

    job post create

--- src/JobBoardManager/index.js
+++ src/JobBoardManager/index.js
@@ -9,10 +9,29 @@ export default class JobBoardManager extends Component {
     }
   }
 
+  // api call to create new job post
+  createJobPost = async (info) => {
+    try {
+      const url = process.env.REACT_APP_API_URL + "api/v1/jobposts/create";
+      const res = await fetch(url, {
+        credentials: 'include',
+        method: "POST",
+        body: JSON.stringify(info),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const json = await res.json();
+      console.log(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
-        <JobPostForm/>
+        <JobPostForm createJobPost={this.createJobPost}/>
       </React.Fragment>
     )
   }
