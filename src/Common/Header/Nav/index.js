198f3de535e7664e2b9fc76d30b0dd7db253ba60
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-07-15 20:30:45 -0500

    more CSS on home page

--- src/Common/Header/Nav/index.js
+++ src/Common/Header/Nav/index.js
@@ -3,16 +3,18 @@ import '../../Common.css'
 
 export default function Nav(props) {
 	return (
-		<div className='Nav'>
-			<props.Link to='/profile/'> profile </props.Link>
-			|
-			<props.Link to='/profile/update'> edit profile </props.Link>
-			|
-			<props.Link to='/login'> login </props.Link>
-			|
-			<props.Link to='/createprofile'> new profile </props.Link>
-			|
-			<props.Link to='/jobs'> jobs </props.Link>
-		</div>
+		<header> 
+			<div className='Nav'>
+				<props.Link to='/profile/'> profile </props.Link>
+				|
+				<props.Link to='/profile/update'> edit profile </props.Link>
+				|
+				<props.Link to='/login'> login </props.Link>
+				|
+				<props.Link to='/createprofile'> new profile </props.Link>
+				|
+				<props.Link to='/jobs'> jobs </props.Link>
+			</div>
+		</header>
 	)
 }
--- src/Common/Home.js
+++ src/Common/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Button, Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import './Common.css'
 
 export default function Home(props){
@@ -38,11 +38,11 @@ export default function Home(props){
 				<div>
 					<div className='JTC'>
 						<h4> Join The Community </h4>
-						<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					</div>
 					<div className='JPA'>
 						<h4> Job Poster Account </h4>
-						<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+						<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
 					</div>
 				</div>
 			</div>
