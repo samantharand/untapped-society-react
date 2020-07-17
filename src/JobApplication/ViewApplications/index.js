a803370c13a58f7979f8ab09339493fa707b564c
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-07-16 20:46:12 -0500

    job posts are collecting in terminal on job page

--- src/JobApplication/ViewApplications/index.js
+++ src/JobApplication/ViewApplications/index.js
@@ -7,12 +7,13 @@ export default class ViewApplication extends Component {
 		super() 
 
 		this.state = {
-			jobApps: []
+			// companies: []
 		}
 	}
 
 	componentDidMount() {
 		console.log('props in JobApplication',this.props);
+		this.getJobs()
 	}	
 
 
@@ -28,14 +29,27 @@ export default class ViewApplication extends Component {
 				credentials: 'include'
 			})
 
-			const getJobsJson = await getJobsRes.json()
 
-			console.log(getJobsRes, getJobsJson);
-			// filter out job posts that do NOT belong to that company - associated w crrent user somehow?
-			const companies = getJobsJson.data
-			const filteredCompanies = await companies.filter(company => company.user.id == this.props.currentUser.id)
+			if(getJobsRes.status === 200) {
+				const getJobsJson = await getJobsRes.json()
 
-			console.log('filteredCompanies', filteredCompanies);
+				console.log(getJobsRes, getJobsJson);
+				// filter out job posts that do NOT belong to that company - associated w crrent user somehow?
+				const companies = getJobsJson.data
+				const filteredCompanies = await companies.filter(company => company.user.id == this.props.currentUser.id)
+
+				console.log('filteredCompanies[0].id', filteredCompanies[0].id);
+
+				this.getJobPostsByCompany(filteredCompanies[0].id)
+
+				// this.getApplications(1)
+
+				// this.setState({
+				// 	companies: filteredCompanies
+				// })
+			} else {
+				console.log("nah man");
+			}
 
 		} catch (error) {
 
@@ -43,13 +57,64 @@ export default class ViewApplication extends Component {
 
 		}
 	}
-				// get all applications on the database from that jobs id -- separate component
+
+	getJobPostsByCompany = async (id) => {
+		console.log(id);
+		const url = process.env.REACT_APP_API_URL + 'api/v1/jobposts/all'
+
+		try {
+			
+			const jobPostsRes = await fetch(url, {
+					credentials: 'include'
+			})
+
+			console.log(jobPostsRes);
+
+			if(jobPostsRes.status === 200) {
+				const jobPostsJson = await jobPostsRes.json()
+
+				console.log(jobPostsJson);
+			}
+			
+		} catch (error) {
+			console.error(error)
+		}
+		// get all jobs posts
+		// filter out based off company id
+	}
+
+	// getApplications = async (id) => {
+	// 	console.log('get applications id', id);
+	// 	try {
+
+	// 		const url = process.env.REACT_APP_API_URL + 'api/v1/jobapplications/all/' + id
+
+	// 		const checkApplicationsRes = await fetch(url, {
+	// 			credentials: 'include'
+	// 		})
+
+	// 		console.log('checkApplicationsRes', checkApplicationsRes);
+
+	// 		// const checkApplicationsJson = await checkApplicationsRes.json()
+	// 		// console.log('checkApplicationsJson', checkApplicationsJson);
+	// 		// if(checkApplicationsRes.status === 200) {
+
+	// 		// 	this.setState({
+	// 		// 		jobApps: checkApplicationsJson
+	// 		// 	})
+	// 		// 	this.checkApplications()
+	// 		// }
+			
+	// 	} catch (error) {
+	// 		console.error(error)
+	// 	}
+	// }
+	// get all applications on the database from that jobs id -- separate component
 
 	// maps out job applications -- another container specficlaly for them? 
 		// accept or deny buttons
 		// accept does something
 		// deny removes from page
-
 
 	render() {
 		return(
